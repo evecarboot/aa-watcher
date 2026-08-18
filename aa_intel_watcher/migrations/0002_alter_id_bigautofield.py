6050d5af3687e01f8d6e429d0509d673cbc48619
evecarboot <greenietv@gmail.com>
2026-08-18 01:48:31 +0100

    Create 0002_alter_id_bigautofield.py

---
+++ aa_intel_watcher/migrations/0002_alter_id_bigautofield.py
@@ -0,0 +1,31 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("aa_intel_watcher", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="chatmessage",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True,
+                primary_key=True,
+                serialize=False,
+                verbose_name="ID",
+            ),
+        ),
+        migrations.AlterField(
+            model_name="streamkey",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True,
+                primary_key=True,
+                serialize=False,
+                verbose_name="ID",
+            ),
+        ),
+    ]
